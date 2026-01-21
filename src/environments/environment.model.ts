a8e398a4f2477a93b914d8113dd55ee1f349572b
Viet Le <tony.leviet@gmail.com>
2026-01-21 23:57:40 +0700

    update env

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -1,7 +1,7 @@
 export abstract class Environment {
   //public apiUrl: string = 'http://192.168.1.164:8080';
-  public apiUrl: string = 'http://localhost:8080';  
-  // public apiUrl: string = 'https://ec2-3-86-143-34.compute-1.amazonaws.com';
+  //public apiUrl: string = 'http://localhost:8080';  
+  public apiUrl: string = 'http://ec2-3-86-143-34.compute-1.amazonaws.com:8000';
   public production: boolean = false;
   public isWebApp: boolean = false;
   public firebase: any;
