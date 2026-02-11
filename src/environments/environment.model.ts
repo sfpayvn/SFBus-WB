8f12b20bd20ee8600249c778cb6a0cdb2e93d891
Viet Le <tony.leviet@gmail.com>
2026-02-12 00:31:27 +0700

    #comment handle sync code to prod

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -2,8 +2,8 @@ export abstract class Environment {
   //public apiUrl: string = 'http://192.168.1.164:8080';
   //public apiUrl: string = 'http://localhost:8080';  
   //public apiUrl: string = 'http://ec2-3-86-143-34.compute-1.amazonaws.com:8000';
-  public apiUrl: string = 'https://ec2-3-86-143-34.compute-1.amazonaws.com';
-  public WEBSOCKET_URL: string = 'https://ec2-3-86-143-34.compute-1.amazonaws.com';
+  public apiUrl: string = 'https://ec2-18-139-117-120.ap-southeast-1.compute.amazonaws.com';
+  public WEBSOCKET_URL: string = 'https://ec2-18-139-117-120.ap-southeast-1.compute.amazonaws.com';
   public production: boolean = false;
   public isWebApp: boolean = false;
   public firebase: any;
